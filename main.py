443f4b88ad9adf0a7fe0785e57d0dfda3be20e65
DanielB-btc <Daniel.Bohnenkamp@btc-ag.com>
2024-09-18 12:01:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ def user_name(name):
     name = input("Enter your name: ")
     return name
 
-
-def ui_field(width, height):
+def ui_input(username, width, height):
+    username = input("Enter your name: ")
     width = input("Enter your width: ")
     height = input("Enter your height: ")
-    return width, height
-
+    return username, width, height
 
