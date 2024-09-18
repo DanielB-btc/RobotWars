504fdacbf07ef9802d672d5e177d85b22a4e9d47
DanielB-btc <Daniel.Bohnenkamp@btc-ag.com>
2024-09-18 11:48:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def user_name(name):
+    name = input("Enter your name: ")
+    return name
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def ui_field(width, height):
+    width = input("Enter your width: ")
+    height = input("Enter your height: ")
+    return width, height
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
