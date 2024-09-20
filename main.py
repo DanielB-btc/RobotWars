5fbb9599bd52ba40fa165e65bcfd02c9214c5ed4
DanielB-btc <Daniel.Bohnenkamp@btc-ag.com>
2024-09-20 15:13:14 +0200

    Bit update - reworked loop

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
-#alt- files
-import AASCII
-import user
-import hub
+run = True
+menu = True
+play = False
+rules = False
 
-#stat menu
-print("Welcome to:")
-print(AASCII.logo())
+HP = 50
+ATK = 3
 
-# robot selection
-hub.main()
+def save():
+    list =[
+        name,
+        str(HP),
+        str(ATK),
 
-#game start
-print(AASCII.board())
-print("Welcome to the map - you are currently at: ", user.pos())
+    ]
 
-print("Testing")
+    f = open("load.txt", "w")
+
+    for item in list:
+        f.write(item + "\n")
+    f.close()
+
+while run:
+    while menu:
+        print("1, NEW GAME")
+        print("2, LOAD GAME")
+        print("3, RULES")
+        print("4, QUIT GAME")
+
+        if rules:
+            print("There are no rules yet")
+            rules = False
+            choice = ""
+            input("> ")
+        else:
+            choice = input("# ")
+
+        if choice == "1":
+            name = input("Name: ")
+            menu = False
+            play = True
+            choice = ""
+            input("> ")
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            rules = True
+        elif choice == "4":
+            quit()
+
+    while play:
+        save()  # autosave
+
+        dest = input("# ")
+
+        if dest == "0":
+            play = False
+            menu = True
+            save()
--- user.py
+++
@@ -1,7 +0,0 @@
-def one():
-    username = input("Enter your username: ")
-    return username
-
-def pos():
-    position = "1 - a"
-    return position
